e199c1ec86d3db41dcd5b1871ed4cc06662ef433
ivanfernm <64442585+ivanfernm@users.noreply.github.com>
2023-07-12 18:34:47 -0300

    Setting the nodes neighbours

--- Assets/NavTerrain.cs
+++ Assets/NavTerrain.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class NavTerrain : MonoBehaviour
@@ -15,6 +16,7 @@ public class NavTerrain : MonoBehaviour
     {
         Init();
         DebugCreateSpheres();
+        _nodes[20].SetNearByNodes();
     }
 
     [ContextMenu("DebugVertexPosition")]
@@ -44,7 +46,7 @@ public class NavTerrain : MonoBehaviour
         //por cada vertice en el mesh va a crear un nodo
         foreach (var vertex in navMesh.vertices)
         {
-            var node = new NavNodes(vertex);
+            var node = new NavNodes(vertex, this);
             _nodes.Add(node);
         }
     }
@@ -52,7 +54,7 @@ public class NavTerrain : MonoBehaviour
     [ContextMenu("debugCreateSpheres")] //esto es para que aparezca en el menu de la ventana de unity
     public void DebugCreateSpheres()
     {
-       Clearspheres();
+        Clearspheres();
         foreach (var node in _nodes)
         {
             //Creo la sphera 
@@ -69,41 +71,89 @@ public class NavTerrain : MonoBehaviour
             _spheres.Add(sphere);
         }
     }
-    
+
     public void Clearspheres()
     {
         foreach (var sphere in _spheres)
         {
             DestroyImmediate(sphere);
         }
+
         _spheres.Clear();
     }
-
 }
 
 public class NavNodes
 {
+    public NavTerrain _navTerrain;
     private Vector3 _vertexPositon;
     private Vector3 _worldPosition;
-
-    public NavNodes(Vector3 vPosition)
+    private List<NavNodes> _nearByNodes = new List<NavNodes>();
+    public NavNodes(Vector3 vPosition, NavTerrain navTerrain)
     {
         _vertexPositon = vPosition;
+        _navTerrain = navTerrain;
     }
 
     public Vector3 GetVertexPosition()
     {
         return _vertexPositon;
     }
-    
+
+    public void SetNearByNodes()
+    {
+        //por cada nodo en la lista de nodos
+        foreach (var node in _navTerrain._nodes)
+        {
+            //si el nodo es el mismo que el que estoy iterando, no hago nada
+            if (node == this)
+            {
+                continue;
+            }
+            //optimise the code bellow
+            var Left = _navTerrain._nodes.Where(x => x.GetWorldPosition().x < _worldPosition.x);
+            if (Left.Any())
+            {
+              var a = Left.OrderByDescending(x => x.GetWorldPosition().x).First();
+              _nearByNodes.Add(a);
+            }
+
+            var right  = _navTerrain._nodes.Where(x => x.GetWorldPosition().x > _worldPosition.x);
+            if (right.Any())
+            {
+               var a = right.OrderByDescending(x => x.GetWorldPosition().x).First();
+               _nearByNodes.Add(a);
+            }
+
+            var up = _navTerrain._nodes.Where(x => x.GetWorldPosition().y > _worldPosition.y);
+            if (up.Any())
+            {
+                var a = up.OrderByDescending(x => x.GetWorldPosition().y).First();
+                _nearByNodes.Add(a);
+            }
+
+            var down = _navTerrain._nodes.Where(x => x.GetWorldPosition().y < _worldPosition.y);
+            if (down.Any())
+            {
+                var a = down.OrderByDescending(x => x.GetWorldPosition().y).First();
+                _nearByNodes.Add(a);
+            }
+
+            
+        }
+        foreach (var VARIABLE in _nearByNodes)
+        {
+            Debug.Log(VARIABLE.GetWorldPosition());
+        }
+    }
+
     public void SetWorldPosition(Vector3 worldPosition)
     {
         _worldPosition = worldPosition;
     }
-    
+
     public Vector3 GetWorldPosition()
     {
         return _worldPosition;
     }
-    
 }
